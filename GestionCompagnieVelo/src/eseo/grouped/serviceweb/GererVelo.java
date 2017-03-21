aa7f82372fa4ffdb0714994e70a91d49158f4fda
chaillcl <clem.chaillou49@gmail.com>
2017-03-21 11:45:36 +0100

    Add files via upload

--- GestionCompagnieVelo/src/eseo/grouped/serviceweb/GererVelo.java
+++ GestionCompagnieVelo/src/eseo/grouped/serviceweb/GererVelo.java
@@ -1,26 +1,61 @@
 package eseo.grouped.serviceweb;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jws.WebService;
+
 public class GererVelo {
 	
 	public Velo[] trouverVelo(Velo unVelo){
-		Velo[] velo = new Velo[10];
-		
+		InterrogationBDD bdd = new InterrogationBDD();
+		bdd.ouvertureConnexion();						//TODO créer fonction ouverture
+		String[][] tableauVelo = bdd.rechercherUnVelo(unVelo); //TODO créer fonction rechercherUnVelo
+		bdd.fermetureConnexion();
+		Velo[] velo = Velo.tableDeVelo(tableauVelo);
 		return velo;
 	}
 	
 	public int ReserverVelo(ReservationVelo reservation){
-		return 0;
+		InterrogationBDD bdd = new InterrogationBDD();
+		bdd.ouvertureConnexion();
+		bdd.reserverUnVelo(reservation.getCodeClient(),reservation.getCodeReservation(),
+					reservation.getCodeVelo(),reservation.getDateDebut(),
+					reservation.getDateFin(),reservation.isPaiementEffectue());
+		bdd.fermetureConnexion(); //TODO créer fonction reserverUnVelo
+		return reservation.getCodeReservation();
 	}
 	
 	public String payerVelo(int codeReservation){
-		return "none";
+		InterrogationBDD bdd = new InterrogationBDD();
+		boolean result = false;
+		
+		bdd.ouvertureConnexion();
+		result = bdd.payerReservationVelo(codeReservation,true); //TODO créer payerReservationVelo
+		bdd.fermetureConnexion();
+		
+		if(result){
+			return "transaction effectué";
+		}else
+			return "la transaction n'a pu aboutir";
+		
+
 	}
 	
-	public boolean AnnulerVelo(int codeReservation){
+	public boolean annulerVelo(int codeReservation){
+		InterrogationBDD bdd = new InterrogationBDD();
+		boolean result = false;
 		
+		bdd.ouvertureConnexion();
+		result = bdd.annulerReservationVelo(codeReservation,true); //TODO créer fonction annulerReservationVelo
+		bdd.fermetureConnexion();
 		
-		return false;
+
+		//TODO faire un try catch et renvoyer un boolean pour dire si la requete est passé 
+		return result;
 	}
+	
+	
 	
 	
 }
